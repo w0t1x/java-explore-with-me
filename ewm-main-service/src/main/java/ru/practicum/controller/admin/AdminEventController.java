23ad90ffdf752a8687be6c2b0f6bedb2b05e630b
Rodion <oberemkoro@yandex.ru>
2026-02-08 21:08:27 +0300

    .,m

--- ewm-main-service/src/main/java/ru/practicum/controller/admin/AdminEventController.java
+++ ewm-main-service/src/main/java/ru/practicum/controller/admin/AdminEventController.java
@@ -39,8 +39,7 @@ public class AdminEventController {
     }
 
     @PatchMapping("/{eventId}")
-    public EventFullDto update(@PathVariable long eventId,
-                               @RequestBody @Valid UpdateEventAdminRequest dto) {
+    public EventFullDto update(@PathVariable long eventId, @RequestBody @Valid UpdateEventAdminRequest dto) {
         return eventService.adminUpdate(eventId, dto);
     }
 }
